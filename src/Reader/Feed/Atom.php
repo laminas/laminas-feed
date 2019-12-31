775ef1086278da93783a799fb2c94ca8ff0da9d9
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:50:09 -0600

    Rewriting as Laminas Project package

--- src/Reader/Feed/Atom.php
+++ src/Reader/Feed/Atom.php
@@ -1,16 +1,15 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-feed for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-feed/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-feed/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Feed\Reader\Feed;
+namespace Laminas\Feed\Reader\Feed;
 
 use DOMDocument;
-use Zend\Feed\Reader;
+use Laminas\Feed\Reader;
 
 /**
 */
--- test/Reader/Entry/RssTest.php
+++ test/Reader/Entry/RssTest.php
@@ -1,20 +1,19 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-feed for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-feed/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-feed/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Feed\Reader\Entry;
+namespace LaminasTest\Feed\Reader\Entry;
 
 use DateTime;
-use Zend\Feed\Reader;
+use Laminas\Feed\Reader;
 
 /**
- * @group Zend_Feed
- * @group Zend_Feed_Reader
+ * @group Laminas_Feed
+ * @group Laminas_Feed_Reader
  */
 class RssTest extends \PHPUnit_Framework_TestCase
 {
@@ -1895,7 +1894,7 @@ class RssTest extends \PHPUnit_Framework_TestCase
             array('/datemodified/plain/none/rss10.xml', null),
             array('/datemodified/plain/none/rss090.xml', null),
 
-            array('/datemodified/plain/rss20-zf-7908.xml', $rss),
+            array('/datemodified/plain/rss20-laminas-7908.xml', $rss),
         );
     }
 
