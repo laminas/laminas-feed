e79381f54dec30654d3552843c1265afffc5a9d3
Marco Pivetta <ocramius@gmail.com>
2022-07-19 10:44:49 +0200

    Adjusted built-in plugin managers for `psr/container:^2` compatibility
    This counts as a minor BC break (which is already inherent of switching to `psr/container:^2`),
    since the return type of `ExtensionManagerInterface#has()` now requires a `bool` return type,
    and so will further child classes.
    
    To mitigate this BC break, the methods for the two `ExtensionManagerInterface` types have been
    moved to the docblock definitions, reducing the chance of inheritance-based BC breaks.
    
    The two `ExtensionManagerInterface` have also been deprecated, since they came from a time where
    `Zend_Loader` (in `zendframework/zendframework1`) was still a thing, whereas `laminas/laminas-servicemanager`
    currently provides healthy abstractions for building objects of a specific category, by using
    cleaner dependency injection patterns.

--- src/Writer/ExtensionManagerInterface.php
+++ src/Writer/ExtensionManagerInterface.php
@@ -4,21 +4,17 @@ declare(strict_types=1);
 
 namespace Laminas\Feed\Writer;
 
+/**
+ * This interface exists to provide type inference for container implementations that retrieve extensions.
+ * Methods have been migrated from declared in PHP to being part of the docblock signature,
+ * in order to avoid conflicting with `psr/container` signatures, which are way stricter, and
+ * therefore incompatible with this one.
+ *
+ * @deprecated this interface is no longer needed, and shouldn't be relied upon
+ *
+ * @method has(string $extension): bool
+ * @method get(string $extension): mixed
+ */
 interface ExtensionManagerInterface
 {
-    /**
-     * Do we have the extension?
-     *
-     * @param  string $extension
-     * @return bool
-     */
-    public function has($extension);
-
-    /**
-     * Retrieve the extension
-     *
-     * @param  string $extension
-     * @return mixed
-     */
-    public function get($extension);
 }
