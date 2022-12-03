3ee6e37966f73379c523909d9126f87eef52b232
Marco Pivetta <ocramius@gmail.com>
2022-12-03 04:20:47 +0100

    Disable camel-case variable declaration checks for closure with unused parameters

--- test/Writer/EntryTest.php
+++ test/Writer/EntryTest.php
@@ -767,11 +767,13 @@ class EntryTest extends TestCase
             'messages' => [],
         ];
 
+        // phpcs:disable WebimpressCodingStandard.NamingConventions.ValidVariableName.NotCamelCaps
         set_error_handler(static function (int $_errno, string $errstr) use ($notices): ?bool {
             $notices->messages[] = $errstr;
 
             return null;
         }, E_USER_NOTICE);
+        // phpcs:enable WebimpressCodingStandard.NamingConventions.ValidVariableName.NotCamelCaps
         new Writer\Entry();
         restore_error_handler();
 
