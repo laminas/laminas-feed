0602129f50c16df5d5436289a57adb27450263c1
Marco Pivetta <ocramius@gmail.com>
2022-12-03 01:52:53 +0100

    Exceptions should always be `Throwable`: marking this component's `ExceptionInterface` as such

--- src/Exception/ExceptionInterface.php
+++ src/Exception/ExceptionInterface.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Laminas\Feed\Exception;
 
-interface ExceptionInterface
+use Throwable;
+
+interface ExceptionInterface extends Throwable
 {
 }
--- src/Writer/Exception/ExceptionInterface.php
+++ src/Writer/Exception/ExceptionInterface.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace Laminas\Feed\Writer\Exception;
 
+use Throwable;
+
 /**
  * Feed exceptions
  *
  * Interface to represent exceptions that occur during Feed operations.
  */
-interface ExceptionInterface
+interface ExceptionInterface extends Throwable
 {
 }
