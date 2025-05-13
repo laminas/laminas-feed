7c6755695cdca18f983d8a116bfa6a55439b1a94
nb823 <102792540+nb823@users.noreply.github.com>
2025-05-13 11:35:51 +0200

    Fix: PodcastIndex feed extension and tests (#98)
    Signed-off-by: naima <naima.blum@podcast.de>

---
+++ test/Writer/Renderer/Extension/PodcastIndex/FeedTest.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LaminasTest\Feed\Writer\Renderer\Extension\PodcastIndex;
+
+use Laminas\Feed\Writer;
+use Laminas\Feed\Writer\Renderer;
+use PHPUnit\Framework\TestCase;
+
+class FeedTest extends TestCase
+{
+    protected Writer\Feed $validWriter;
+
+    protected function setUp(): void
+    {
+        Writer\Writer::reset();
+
+        $this->validWriter = new Writer\Feed();
+        $this->validWriter->setTitle('This is a test feed.');
+        $this->validWriter->setDescription('This is a test description.');
+        $this->validWriter->setLink('http://www.example.com');
+        $this->validWriter->setType('rss');
+    }
+
+    protected function tearDown(): void
+    {
+        Writer\Writer::reset();
+    }
+
+    public function testRendersLockedTag(): void
+    {
+        $locked = [
+            'value' => 'yes',
+            'owner' => 'john.doe@example.com',
+        ];
+        $this->validWriter->setPodcastIndexLocked($locked);
+
+        $rssFeed = new Renderer\Feed\Rss($this->validWriter);
+        $xml     = $rssFeed->render()->saveXml();
+        $this->assertStringContainsString('<podcast:locked', $xml);
+    }
+
+    public function testRendersFundingTag(): void
+    {
+        $funding = [
+            'title' => 'Support the show!',
+            'url'   => 'http://example.com/donate',
+        ];
+        $this->validWriter->setPodcastIndexFunding($funding);
+
+        $rssFeed = new Renderer\Feed\Rss($this->validWriter);
+        $xml     = $rssFeed->render()->saveXml();
+
+        $this->assertStringContainsString('<podcast:funding', $xml);
+    }
+}
